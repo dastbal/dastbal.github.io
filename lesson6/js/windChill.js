c6dcfb338867ee80c7555990426cb90791be395f
dastballadares <bal20001@byui.edu>
2021-02-09 20:50:56 -0500

    adding windChil.js

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,13 +1,13 @@
-let speed = document.getElementById("speed")
-let high = parseInt(document.getElementById("high"))
+let speed = parseInt(document.getElementById("speed").innerHTML)
+let high = parseInt(document.getElementById("high").innerHTML)
 let windChill = document.getElementById("windChill")
 console.log(speed)
-const wd = ( speed, high) =>{
+const wd = ( speed, high ) =>{
     let s = speed
     let t = high
     return (35.74 + 0.6215 *t - 35.75 * ( s ** 0.16) + 0.4275 * t* (s** 0.16))
 }
-let output = wd(5,6).toFixed(1)
+let output = wd(speed,high).toFixed(1) 
 console.log(output)
 
 windChill.textContent = output

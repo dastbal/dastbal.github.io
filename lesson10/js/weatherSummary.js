a388c41dd7454886d4b58a61d8cea011b8cb9bd7
dastballadares <bal20001@byui.edu>
2021-03-10 16:21:00 -0500

    adding  css to the forecast

--- lesson10/js/weatherSummary.js
+++ lesson10/js/weatherSummary.js
@@ -9,21 +9,21 @@ let speed = document.getElementById('speed')
 let windChill = document.getElementById("windChill")
 
 
-const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityId}&units=imperial&appid=${apiKey}`;
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=imperial&appid=${apiKey}`;
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
     
     
-    d.textContent = jsObject.list[0].weather[0].description
-    temp.textContent = jsObject.list[0].main.temp
-    highTemp.textContent = jsObject.list[0].main.temp_max
-    humidity.textContent = jsObject.list[0].main.humidity
-    speed.textContent = jsObject.list[0].wind.speed
+    d.textContent = jsObject.weather[0].description
+    temp.textContent = jsObject.main.temp
+    highTemp.textContent = jsObject.main.temp_max
+    humidity.textContent = jsObject.main.humidity
+    speed.textContent = jsObject.wind.speed
 
-  let s = jsObject.list[0].wind.speed
-  let t = jsObject.list[0].main.temp
+  let s = jsObject.wind.speed
+  let t = jsObject.main.temp
   const wd = ( s, t) => (35.74 + 0.6215 *t - 35.75 * ( s ** 0.16) + 0.4275 * t* (s** 0.16))
     
   let output = wd(s,t).toFixed(1) 

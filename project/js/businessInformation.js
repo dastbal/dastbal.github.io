220710cf00bb893340a37af4de427b2d429f1220
dastballadares <bal20001@byui.edu>
2021-04-02 02:14:34 -0500

    updating the json

--- project/js/businessInformation.js
+++ project/js/businessInformation.js
@@ -1,14 +1,33 @@
  async function  businessInformation(){
      let businesses= document.querySelector('.Business-companies')
-     const response = await fetch('/project/directory.html')
+     let companies =[]
+
+
+
+     const response = await fetch('https://dastbal.github.io/project/js/cities.json')
      const  data = await response.json()
      console.log(data)
-     data['companies'].forEach( company => {
-        let div = document.createElement('div')
-        let h2 = document.createElement('h2')
-        h2.innerHTML = company.name
-        div.append(h2)
-    } )
-    businesses.append(business)
 
+
+
+
+     data['companies'].forEach( company => {
+        let container = document.createElement('div')
+        let h2 = document.createElement('h2')
+
+
+
+
+        h2.innerHTML = company.name
+
+
+
+
+        container.append(h2)
+        companies.push(container)
+    } )
+    
+    businesses.append(...companies)
 }
+
+businessInformation()
